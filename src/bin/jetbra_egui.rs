9d15d1c6f9a8364737b5d0732f988d80413ef73a
Yanchen <yanchen1610@gmail.com>
2023-05-05 14:20:21 +0800

    Update egui UI.

--- src/bin/jetbra_egui.rs
+++ src/bin/jetbra_egui.rs
@@ -1,8 +1,8 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
 
 use anyhow::Result;
-use eframe::egui;
 use eframe::egui::{ComboBox, Ui};
+use eframe::{egui, Theme};
 
 use jetbra::app::{App, Apps};
 use jetbra::install::{InstallArgs, Installer};
@@ -12,14 +12,16 @@ use jetbra::uninstall::{UninstallArgs, Uninstaller};
 fn main() -> Result<()> {
     let options = eframe::NativeOptions {
         initial_window_size: Some(egui::vec2(320.0, 240.0)),
+        resizable: false,
+        default_theme: Theme::Light,
         ..Default::default()
     };
-    let app = JetbraApp::new()?;
+    let app = Jetbra::new()?;
     eframe::run_native("Jetbra", options, Box::new(|_cc| Box::from(app))).unwrap();
     Ok(())
 }
 
-struct JetbraApp {
+struct Jetbra {
     apps: Vec<Apps>,
     installer: Installer,
     uninstaller: Uninstaller,
@@ -27,7 +29,7 @@ struct JetbraApp {
     selected_app: Option<Apps>,
 }
 
-impl JetbraApp {
+impl Jetbra {
     fn new() -> Result<Self> {
         let jetbrains_dir = jetbrains::path()?;
         Ok(Self {
@@ -38,27 +40,20 @@ impl JetbraApp {
         })
     }
 
-    fn select_app(&mut self, ui: &mut Ui) -> Result<()> {
-        ui.horizontal(|ui| {
-            ComboBox::from_label("App")
-                .selected_text(match &self.selected_app {
-                    None => "All".to_owned(),
-                    Some(app) => App::from(*app).name,
-                })
-                .show_ui(ui, |ui| {
-                    // All
-                    ui.selectable_value(&mut self.selected_app, None, "All");
-                    // Apps
-                    self.apps.iter().for_each(|app| {
-                        ui.selectable_value(
-                            &mut self.selected_app,
-                            Some(*app),
-                            App::from(*app).name.as_str(),
-                        );
-                    });
+    fn select_app(&mut self, ui: &mut Ui) {
+        ComboBox::from_label("")
+            .selected_text(match &self.selected_app {
+                None => "All".to_owned(),
+                Some(app) => App::from(*app).name,
+            })
+            .show_ui(ui, |ui| {
+                // All
+                ui.selectable_value(&mut self.selected_app, None, "All");
+                // Apps
+                self.apps.iter().for_each(|app| {
+                    ui.selectable_value(&mut self.selected_app, Some(*app), App::from(*app).name);
                 });
-        });
-        Ok(())
+            });
     }
 
     fn install(&self, ui: &mut egui::Ui) -> Result<()> {
@@ -80,13 +75,11 @@ impl JetbraApp {
     }
 }
 
-impl eframe::App for JetbraApp {
+impl eframe::App for Jetbra {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         egui::CentralPanel::default().show(ctx, |ui| {
             // Select app
-            if let Err(err) = self.select_app(ui) {
-                ui.label(err.to_string());
-            }
+            self.select_app(ui);
             // Install
             if let Err(err) = self.install(ui) {
                 ui.label(err.to_string());
