099e5bca4bd20aff41aa88692d1f4e1a43d1347d
Yanchen Chen <yanchen1610@gmail.com>
2023-06-10 22:36:42 +0800

    Support Windows and Linux, but not confirmed JetBrains path.

--- src/jetbrains.rs
+++ src/jetbrains.rs
@@ -4,14 +4,25 @@ use anyhow::{anyhow, Result};
 
 /// Return the path of JetBrains directory.
 /// macOS: ~/Library/Application Support/JetBrains
-/// TODO confirm: Linux: ~/.config/JetBrains
-/// TODO confirm: Windows: %APPDATA%\JetBrains
+#[cfg(target_os = "macos")]
 pub fn path() -> Result<PathBuf> {
-    let home_dir = dirs::home_dir().ok_or(anyhow!("cannot find home directory"))?;
-    // TODO: support Linux and Windows
-    let jetbrains_dir = home_dir
-        .join("Library")
-        .join("Application Support")
-        .join("JetBrains");
+    let home_dir = dirs::config_dir().ok_or(anyhow!("cannot find home directory"))?;
+    let jetbrains_dir = home_dir.join("JetBrains");
+    Ok(jetbrains_dir)
+}
+
+// TODO: Make sure it's correct.
+#[cfg(target_os = "linux")]
+pub fn path() -> Result<PathBuf> {
+    let home_dir = dirs::config_dir().ok_or(anyhow!("cannot find home directory"))?;
+    let jetbrains_dir = home_dir.join("JetBrains");
+    Ok(jetbrains_dir)
+}
+
+// TODO: Make sure the right path of JetBrains directory on Windows.
+#[cfg(target_os = "windows")]
+pub fn path() -> Result<PathBuf> {
+    let home_dir = dirs::config_dir().ok_or(anyhow!("cannot find home directory"))?;
+    let jetbrains_dir = home_dir.join("JetBrains");
     Ok(jetbrains_dir)
 }
